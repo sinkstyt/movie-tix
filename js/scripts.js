d0199457db8f1608b583c0e7fd46169f9d3c590d
Tyler Sinks <tyler.sinks@gmail.com>
2021-01-26 18:19:51 -0500

    add calcPrice logic and pass all specs
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 function Ticket(isFirstRelease, timeOfDay, ageYears, title) {
-  this.isFirstRelease = false;
+  this.isFirstRelease = isFirstRelease;
   this.timeOfDay = timeOfDay;
   this.age = ageYears;
   this.title = title;
 }
 
 Ticket.prototype.calculatePrice = function() {
-  let price = 14;
-  if (this.age >= 65) {
-    price -= 4;
+  let price;
+  if (this.isFirstRelease == true) {
+    price = 20;
+    console.log("got into expensive block of code!");
+    return price;
+  } else if (this.age >= 65 || this.timeOfDay < 1300) {
+    price = 10;
+  } else {
+    price = 14;
   }
   return price;
 }
